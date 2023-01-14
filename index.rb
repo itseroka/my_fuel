a94d84685a5027dcbaeac4d7e65a989be6777dc2
itseroka <it.seroka@gmail.com>
2023-01-14 20:17:03 +0100

    kalkulator normy działa

--- index.rb
+++ index.rb
@@ -1,12 +1,12 @@
 puts "Podaj stan licznika"
 
-@przed = gets.chomp.to_i
+@przed = gets.chomp.to_f
 
 puts "Podaleś liczbe #{@przed} \n" 
 
 puts "podaj stan licznika po"
 
-@po = gets.chomp.to_i
+@po = gets.chomp.to_f
 
 puts "Podałeś liczbe #{@po}"
 
@@ -14,11 +14,12 @@ puts "Przejechane kilometry to #{@po - @przed} \n"
 
 puts "Podaj stan paliwa przed"
 
-@start = gets.chomp.to_i
+@start = gets.chomp.to_f
 
 puts "Podaj stan paliwa po powrocie"
 
-@koniec = gets.chomp.to_i
+@koniec = gets.chomp.to_f
 
 puts "Zurzyte paliwo to #{@start - @koniec}"
 
+puts "Twoja norma na 100 km to #{((@start - @koniec)/(@po - @przed))*100}"
