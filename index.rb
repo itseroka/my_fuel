90301d9523eb8c7d02c2642ed9767b3c6945039f
itseroka <it.seroka@gmail.com>
2023-01-14 19:24:27 +0100

    dodawanie liczb dziala

---
+++ index.rb
@@ -0,0 +1,13 @@
+puts "Podaj liczbe A"
+
+@liczba = gets.chomp.to_i
+
+print "Podaleś liczbe #{@liczba} "
+
+puts "podaj liczbe B"
+
+@druga = gets.chomp.to_i
+
+print "Podałeś liczbe #{@druga}"
+
+print "Suma liczb A i B to #{@liczba + @druga}"
