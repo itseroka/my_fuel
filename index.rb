dedefbd5a0dc41a89a4ced2cfc8e213dc381e707
itseroka <it.seroka@gmail.com>
2023-01-30 18:51:24 +0100

    dodano generowanie wyliczenia normy do pliku csv

--- index.rb
+++ index.rb
@@ -1,6 +1,6 @@
 # kalkulator normy spalania
 # uruchom za pomocą komendy $ ruby index.rb
-
+require 'csv'
 # wartości - paliwo
 @a = 0
 @b = 0
@@ -11,35 +11,60 @@
 
 # czyszczenie konsoli
 system("clear")
+
+puts "Witaj w kalkulatorze norm spalania, bdziesz poproszony o numer rejestracyjny pojazdu, date wyjazdu, datę powrotu, stan początkowy paliwa, dodatnkowania oraz stan końcowy paliwa żeby obliczyć normę zużycia paliwa l/100 km"
+
+puts "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+
+puts "Podaj numer rejestracyjny pojazdu"
+@pojazd = gets.chomp
+
+puts "Podaj datę wyjazdu - przykład 01.02 [rok doda się automatycznie]"
+@data = gets.chomp
+@data = "#{@data}.2022"
+
+puts "Podaj datę zjazdu - przykład 09.02 [rok doda się automatycznie]"
+@zjazd = gets.chomp
+@zjazd = "#{@zjazd}.2022"
+
 puts "Podaj stan licznika przy wyjeździe w trase"
 @przed = gets.chomp.to_f
 
 puts "Podaj stan licznika po powrocie na baze"
 @po = gets.chomp.to_f
-
 @przejechanekilometry = @po - @przed
 
 puts "Podaj stan paliwa podczas startu"
 @a = gets.chomp.to_f
 
-puts "czy bylo dotankowanie? tak/nie"
+puts "Czy bylo dotankowanie? tak/nie"
 until @petla == "nie"
   @petla = gets.chomp
     if @petla == "tak"
     puts "Podaj liczbę do dodania"
     @b = @b + gets.chomp.to_f
-    puts "czy chcesz dodać więcej? TAK/NIE"
-    puts "twoje dotankowania to #{@b}"
+    puts "Twoje dotankowania to #{@b}"
+    puts "Czy chcesz dodać więcej? [tak/nie]"
   end
 end
-
 
 puts "Podaj stan paliwa po powrocie"
 @c = gets.chomp.to_f
 
+@zuzycie = @a + @b - @c
+@norma = (@zuzycie/@przejechanekilometry)*100
+
+puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+puts "Twój pojazd to #{@pojazd}"
+puts "Data wyjazdu: #{@data} / Data zjazdu: #{@zjazd}"
 puts "Przejechane kilometry to #{@przejechanekilometry}"
 puts "Zurzyte paliwo to #{@a + @b - @c}"
+puts "Twoja norma na 100 km to #{@norma}"
+puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-@zuzycie = @a + @b - @c
+CSV.open("#{@pojazd}#{@data}#{@zjazd}.csv", "w") do |csv|
+  csv << ["Pojazd", "Data wyjazdu", "Data powrotu", "Dotankowane paliwo", "Zużyte paliwo", "Norma l/100km"]
+  csv << ["#{@pojazd}", "#{@data}", "#{@zjazd}", "#{@b}", "#{@zuzycie}", "#{@norma}"]
+end
 
-puts "Twoja norma na 100 km to #{(@zuzycie/@przejechanekilometry)*100}"
+puts "Zapisano raport #{@pojazd}#{@data}#{@zjazd}.csv"
