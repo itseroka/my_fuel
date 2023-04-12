6ec887971260010fee005c1bd9f84f0a4e2ff66b
itseroka <it.seroka@gmail.com>
2023-04-12 21:02:23 +0200

    dane do rej wyjazdu

--- cli_proces.rb
+++ cli_proces.rb
@@ -1,37 +1,69 @@
+require_relative "przejazd_proces"
+
+# def cli_zarejestruj_wyjazd
+#  puts "Podaj numer rejestracyjny pojazdu"
+#  @numer_pojazdu = gets.chomp.upcase
+   
+#  puts "Podaj datę wyjazdu"
+#  @data_wyjazdu = gets.chomp
+   
+#  puts "Podaj stan licznika podczas wyjazdu w trasę"
+#  @km_wyjazd = gets.chomp.to_f
+# end
+   
+# def cli_zarejestruj_dodatnkowanie
+#  puts "Czy podczas wyjazdu było dotankowywane paliwo? Podaj: TAK/NIE"
+#  @paliwo_dodatkowo = 0
+#   until @tankowania_w_trasie == "NIE"
+#   @tankowania_w_trasie = gets.chomp.upcase
+#    if @tankowania_w_trasie == "TAK"
+#    puts "Podaj wartość zatankowanego paliwa"
+#    @paliwo_dodatkowo = @paliwo_dodatkowo + gets.chomp.to_f
+#    puts "Czy było kolejne tankowanie? Podaj: TAK/NIE"
+#    end
+#   end
+# end
+   
+# def cli_zarejestruj_powrot
+#  puts "Podaj datę zjazdu"
+#  @data_zjazdu = gets.chomp
+
+#  puts "Podaj stan licznika po powrocie z trasy"
+#  @km_powrot = gets.chomp.to_f
+   
+#  puts "Podaj stan paliwa po powrocie z trasy"
+#  @paliwo_zjazd = gets.chomp.to_f
+# end
+   
+# #    def wypisz_norme
+# #     #    norma z csv
+# #    end
+
+#    cli_zarejestruj_wyjazd
+# #    cli_zarejestruj_dodatnkowanie
+# #    cli_zarejestruj_powrot
+
+#    przejazd_proces = Przejazd_proces.new(@numer_pojazdu)
+
+#    puts przejazd_proces.numer_pojazdu
+
+
 def cli_zarejestruj_wyjazd
-    puts "Podaj numer rejestracyjny pojazdu"
-    @numer_pojazdu = gets.chomp.upcase
-   
-    puts "Podaj datę wyjazdu"
-    @data_wyjazdu = gets.chomp
-   
-    puts "Podaj stan licznika podczas wyjazdu w trasę"
-    @km_wyjazd = gets.chomp.to_f
-   end
-   
-   def cli_zarejestruj_dodatnkowanie
-       puts "Czy podczas wyjazdu było dotankowywane paliwo? Podaj: TAK/NIE"
-     @paliwo_dodatkowo = 0
-      until @tankowania_w_trasie == "NIE"
-       @tankowania_w_trasie = gets.chomp.upcase
-        if @tankowania_w_trasie == "TAK"
-         puts "Podaj wartość zatankowanego paliwa"
-         @paliwo_dodatkowo = @paliwo_dodatkowo + gets.chomp.to_f
-         puts "Czy było kolejne tankowanie? Podaj: TAK/NIE"
-        end
-   end
-   
-   def cli_zarejestruj_powrot
-       puts "Podaj datę zjazdu"
-       @data_zjazdu = gets.chomp
-   
-       puts "Podaj stan licznika po powrocie z trasy"
-       @km_powrot = gets.chomp.to_f
-   
-       puts "Podaj stan paliwa po powrocie z trasy"
-       @paliwo_zjazd = gets.chomp.to_f
-   end
-   
-   def wypisz_norme
-    #    norma z csv
-   end
+  puts "Podaj numer rejestracyjny pojazdu"
+  @numer_pojazdu = gets.chomp.upcase
+
+  puts "Podaj datę wyjazdu"
+  @data_wyjazdu = gets.chomp
+
+  puts "Podaj stan licznika"
+  @km_wyjazd = gets.chomp.to_f
+end
+
+cli_zarejestruj_wyjazd
+
+przejazd_proces = Przejazd_proces.new(@numer_pojazdu, @data_wyjazdu, @km_wyjazd)
+
+puts "1. #{przejazd_proces.numer_pojazdu}"
+puts "2. #{przejazd_proces.data_wyjazdu}"
+puts "3. #{przejazd_proces.km_wyjazd}"
+
--- przejazd_proces.rb
+++ przejazd_proces.rb
@@ -1,39 +1,11 @@
 require 'csv'
 
-# class Przejazd
-
-#     def initialize(numer_pojazdu)
-#         @numer_pojazdu = numer_pojazdu
-#     end
-
-#     def wyjazd(data_wyjazdu, km_wyjazd, paliwo_wyjazd)
-#         @data_wyjazdu = data_wyjazdu
-#         @km_wyjazd = km_wyjazd
-#         @paliwo_wyjazd = paliwo_wyjazd
-#     end
-
-#     def dotankowanie(paliwo_dodatkowo)
-#         @paliwo_dodatkowo = paliwo_dodatkowo
-#     end
-
-#     def powrot(data_zjazdu, km_powrot, paliwo_zjazd)
-#         @data_zjazdu = data_zjazdu
-#         @km_powrot = km_powrot
-#         @paliwo_zjazd = paliwo_zjazd
-#     end
-
-#     def norma_spalania
-#         @zuzycie = @paliwo_wyjazd + @paliwo_dodatkowo - @paliwo_zjazd
-#         @przejechane_kilometry = @km_powrot - @km_wyjazd
-#         @norma = (@zuzycie/@przejechane_kilometry)*100
-#         return @norma
-#        end
-
-# end
-
-
-
 class Przejazd_proces
-
-
-end
+    attr_accessor :numer_pojazdu, :data_wyjazdu, :km_wyjazd
+    def initialize(numer_pojazdu, data_wyjazdu, km_wyjazd)
+      @numer_pojazdu = numer_pojazdu
+      @data_wyjazdu = data_wyjazdu
+      @km_wyjazd = km_wyjazd
+    end
+  end
+  
