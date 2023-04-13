9d891fffdcb18b78a710bb553d6104900d9cdd83
itseroka <it.seroka@gmail.com>
2023-04-13 21:41:56 +0200

    więcej danych do przekazania - test ok

--- cli_proces.rb
+++ cli_proces.rb
@@ -59,11 +59,35 @@ def cli_zarejestruj_wyjazd
   @km_wyjazd = gets.chomp.to_f
 end
 
-cli_zarejestruj_wyjazd
+def cli_zarejestruj_dodatnkowanie
+  puts "Podaj ilość dotankowanego paliwa"
+  @paliwo_dodatkowo = gets.chomp.to_f
+end
 
-przejazd_proces = Przejazd_proces.new(@numer_pojazdu, @data_wyjazdu, @km_wyjazd)
+def cli_zarejestruj_powrot
+  puts "Podaj datę zjazdu"
+  @data_zjazdu = gets.chomp
+ 
+  puts "Podaj stan licznika po powrocie z trasy"
+  @km_powrot = gets.chomp.to_f
+    
+  puts "Podaj stan paliwa po powrocie z trasy"
+  @paliwo_zjazd = gets.chomp.to_f
+ end
+
+cli_zarejestruj_wyjazd
+cli_zarejestruj_dodatnkowanie
+cli_zarejestruj_powrot
+
+ przejazd_proces = Przejazd_proces.new(@numer_pojazdu, @data_wyjazdu, @km_wyjazd, @paliwo_dodatkowo, @data_zjazdu, @km_powrot, @paliwo_zjazd)
 
 puts "1. #{przejazd_proces.numer_pojazdu}"
 puts "2. #{przejazd_proces.data_wyjazdu}"
 puts "3. #{przejazd_proces.km_wyjazd}"
+
+puts "4. #{przejazd_proces.paliwo_dodatkowo}"
+
+puts "5. #{przejazd_proces.data_zjazdu}"
+puts "6. #{przejazd_proces.km_powrot}"
+puts "7. #{przejazd_proces.paliwo_zjazd}"
 
--- przejazd_proces.rb
+++ przejazd_proces.rb
@@ -1,11 +1,16 @@
 require 'csv'
 
 class Przejazd_proces
-    attr_accessor :numer_pojazdu, :data_wyjazdu, :km_wyjazd
-    def initialize(numer_pojazdu, data_wyjazdu, km_wyjazd)
+    attr_accessor :numer_pojazdu, :data_wyjazdu, :km_wyjazd, :paliwo_dodatkowo, :data_zjazdu, :km_powrot, :paliwo_zjazd
+    def initialize(numer_pojazdu, data_wyjazdu, km_wyjazd, paliwo_dodatkowo, data_zjazdu, km_powrot, paliwo_zjazd)
       @numer_pojazdu = numer_pojazdu
       @data_wyjazdu = data_wyjazdu
       @km_wyjazd = km_wyjazd
+      @paliwo_dodatkowo = paliwo_dodatkowo
+      @data_zjazdu = data_zjazdu
+      @km_powrot = km_powrot
+      @paliwo_zjazd = paliwo_zjazd
     end
+
   end
   
