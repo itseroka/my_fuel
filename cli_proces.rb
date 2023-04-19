4491b3ed7b6a16c5b6068929c61f84820826b2af
itseroka <it.seroka@gmail.com>
2023-04-19 19:22:48 +0200

    metoda wyjazd

--- cli_proces.rb
+++ cli_proces.rb
@@ -9,6 +9,9 @@ def cli_zarejestruj_wyjazd
 
   puts "Podaj stan licznika"
   @km_wyjazd = gets.chomp.to_f
+
+  puts "Podaj stan paliwa przy wyjeździe w trasę"
+  @paliwo_wyjazd = gets.chomp.to_f
 end
 
 def cli_zarejestruj_dodatnkowanie
@@ -31,7 +34,7 @@ cli_zarejestruj_wyjazd
 cli_zarejestruj_dodatnkowanie
 cli_zarejestruj_powrot
 
- przejazd_proces = Przejazd_proces.new(@numer_pojazdu, @data_wyjazdu, @km_wyjazd, @paliwo_dodatkowo, @data_zjazdu, @km_powrot, @paliwo_zjazd)
+ przejazd_proces = Przejazd_proces.new(@numer_pojazdu, @data_wyjazdu, @km_wyjazd, @paliwo_wyjazd, @paliwo_dodatkowo, @data_zjazdu, @km_powrot, @paliwo_zjazd)
 
 puts "1. #{przejazd_proces.numer_pojazdu}"
 puts "2. #{przejazd_proces.data_wyjazdu}"
@@ -42,4 +45,6 @@ puts "4. #{przejazd_proces.paliwo_dodatkowo}"
 puts "5. #{przejazd_proces.data_zjazdu}"
 puts "6. #{przejazd_proces.km_powrot}"
 puts "7. #{przejazd_proces.paliwo_zjazd}"
+
+przejazd_proces.wyjazd
 
--- przejazd_proces.rb
+++ przejazd_proces.rb
@@ -1,15 +1,25 @@
 require 'csv'
 
 class Przejazd_proces
-    attr_accessor :numer_pojazdu, :data_wyjazdu, :km_wyjazd, :paliwo_dodatkowo, :data_zjazdu, :km_powrot, :paliwo_zjazd
-    def initialize(numer_pojazdu, data_wyjazdu, km_wyjazd, paliwo_dodatkowo, data_zjazdu, km_powrot, paliwo_zjazd)
+    attr_accessor :numer_pojazdu, :data_wyjazdu, :km_wyjazd, :paliwo_dodatkowo, :paliwo_wyjazd, :data_zjazdu, :km_powrot, :paliwo_zjazd
+    def initialize(numer_pojazdu, data_wyjazdu, km_wyjazd, paliwo_wyjazd, paliwo_dodatkowo, data_zjazdu, km_powrot, paliwo_zjazd)
       @numer_pojazdu = numer_pojazdu
       @data_wyjazdu = data_wyjazdu
       @km_wyjazd = km_wyjazd
+      @paliwo_wyjazd = paliwo_wyjazd
       @paliwo_dodatkowo = paliwo_dodatkowo
       @data_zjazdu = data_zjazdu
       @km_powrot = km_powrot
       @paliwo_zjazd = paliwo_zjazd
+    end
+
+    def wyjazd
+      @raport_path = "progress/#{@numer_pojazdu}.csv"
+
+      CSV.open(@raport_path, "w+") do |csv|
+        csv << ["Data wyjazdu", "Sstan licznika podczas wyjazdu", "Stan paliwa podczas wyjazdu"]
+        csv << [@data_wyjazdu, @km_wyjazd, @paliwo_wyjazd]
+       end
     end
 
   end
